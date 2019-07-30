9d4fb87053abab0fdf98ee7470d0a7f75b06f642
Vladimir <ivannikov.volod@gmail.com>
2019-07-30 21:18:30 +0300

    add task 3101

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -1,35 +1,57 @@
 package com.javarush.task.task31.task3101;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Map;
 import java.util.TreeMap;
 
 /*
 Проход по дереву файлов
 */
+class MyFileVisitor extends SimpleFileVisitor<Path>{
+//    TreeMap<String, File> treeMap = new TreeMap<>();
+//    @Override
+//    public FileVisitResult visitFile (Path path, BasicFileAttributes attr) throws IOException{
+//        if (Files.exists(path)&& path.toFile().length() <= 50){
+//           treeMap.put(path.getFileName().toString(), path.toFile());
+//        }
+//        return FileVisitResult.CONTINUE;
+//    }
+
+}
 public class Solution {
     public static void main(String[] args) {
         TreeMap<String, File> treeMap = new TreeMap<>();
-        File path = new File(args[0]);
         File resultFileAbsolutePath = new File(args[1]);
-
-        public static void fileTree(File path, TreeMap<String, File> treeMap){
-            if (path.isDirectory()){
-                for (File entry:path.listFiles()) {
-                    if (entry.length() <= 50 && entry.isFile()){
-                        treeMap.put(entry.getName(), entry);
-                    }else if (entry.isDirectory()){
-                        fileTree(entry, treeMap);
-                    }
-                }
-            }
-        }
-
-
-
         File newFileName = new File(resultFileAbsolutePath.getParent() + File.separator + "allFilesContent.txt");
         FileUtils.renameFile(resultFileAbsolutePath, newFileName);
 
+        Path path = Paths.get(args[0]);
 
+        try (FileOutputStream fos = new FileOutputStream(newFileName)){
+            Files.walkFileTree(path, new SimpleFileVisitor<Path>(){
+                @Override
+                public FileVisitResult visitFile (Path path, BasicFileAttributes attr) throws IOException{
+                    if (Files.exists(path)&& path.toFile().length() <= 50){
+                        treeMap.put(path.getFileName().toString(), path.toFile());
+                    }
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+            for (Map.Entry<String, File> item:treeMap.entrySet()) {
+                FileInputStream fis = new FileInputStream(item.getValue());
+
+                byte[] buffer = new byte[fis.available()];
+                fis.read(buffer, 0, fis.available());
+                fis.close();
+                fos.write(buffer, 0 , buffer.length);
+                fos.write(10);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
