a010f7d58b451aead972d38815c3324f975d2f15
Vladimir <ivannikov.volod@gmail.com>
2019-08-03 15:27:28 +0300

    add task 3104

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3104/Solution.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3104/Solution.java
@@ -15,7 +15,8 @@ public class Solution extends SimpleFileVisitor<Path> {
     public static void main(String[] args) throws IOException {
         EnumSet<FileVisitOption> options = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
         final Solution solution = new Solution();
-        Files.walkFileTree(Paths.get("D:/"), options, 20, solution);
+//        Files.walkFileTree(Paths.get("D:/"), options, 20, solution);
+        Files.walkFileTree(Paths.get("D:\\Уроки\\JavaRushTasks\\JavaRushTasks\\4.JavaCollections\\src\\com\\javarush\\task\\task31\\task3104\\Test"), options, 20, solution);
 
         List<String> result = solution.getArchived();
         System.out.println("All archived files:");
@@ -28,10 +29,12 @@ public class Solution extends SimpleFileVisitor<Path> {
         for (String path : failed) {
             System.out.println("\t" + path);
         }
+
     }
 
     private List<String> archived = new ArrayList<>();
     private List<String> failed = new ArrayList<>();
+
 
     public List<String> getArchived() {
         return archived;
@@ -39,5 +42,26 @@ public class Solution extends SimpleFileVisitor<Path> {
 
     public List<String> getFailed() {
         return failed;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+//        String pattern = "glob:*.zip";
+        String pattern = "glob:*.zip";
+        String pattern2 = "glob:*.rar";
+//        archived.add(String.valueOf(file));
+        PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher(pattern);
+        PathMatcher pathMatcher2 = FileSystems.getDefault().getPathMatcher(pattern2);
+        if (pathMatcher.matches(file.getFileName()) || pathMatcher2.matches(file.getFileName())){
+            archived.add(String.valueOf(file));
+        }
+
+        return super.visitFile(file, attrs);
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        failed.add(file.toString());
+        return FileVisitResult.SKIP_SUBTREE;
     }
 }
