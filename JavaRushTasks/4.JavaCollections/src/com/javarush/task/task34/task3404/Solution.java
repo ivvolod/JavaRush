6ef73cf216526191749c5f78f2d592431b38aa79
Vladimir <ivannikov.volod@gmail.com>
2019-12-02 08:11:45 +0300

    task 3405

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task34/task3404/Solution.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task34/task3404/Solution.java
@@ -1,6 +1,12 @@
 package com.javarush.task.task34.task3404;
 
-/* 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.LineNumberReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
 Рекурсия для мат. выражения
 */
 public class Solution {
@@ -11,9 +17,67 @@ public class Solution {
 
     public void recurse(final String expression, int countOperation) {
         //implement
+        if (expression.startsWith("(") && expression.endsWith(")")) {
+            recurse(expression.substring(1, expression.length() - 1), countOperation);
+        } else if (!expression.startsWith("(") && expression.endsWith(")")) {
+
+            while (countOperation < expression.length() - 1) {
+                countOperation++;
+                StringBuffer stringBuffer = new StringBuffer();
+                if (Character.isAlphabetic(expression.charAt(countOperation))) {
+                    stringBuffer.append(expression.charAt(countOperation));
+                }
+                String operation = stringBuffer.toString();
+
+
+            }
+
+        } else if (expression.startsWith("sin(") && expression.endsWith(")")) {
+            recurse(expression.substring(4, expression.length() - 1), countOperation);
+        } else {
+            StringBuffer stringBuffer = new StringBuffer();
+            while (countOperation < expression.length() - 1) {
+                if (Character.isDigit(expression.charAt(countOperation))
+                        || expression.charAt(countOperation) == '.'
+                        || expression.charAt(countOperation) == '-') {
+                    stringBuffer.append(expression.charAt(countOperation));
+                    countOperation++;
+                }
+                double leftOperand = Double.parseDouble(stringBuffer.toString());
+
+
+//                if (expression.charAt(countOperation) == operation)
+                char operation = expression.charAt(countOperation);
+
+                recurse(expression.substring(countOperation + 1), countOperation);
+
+//                switch (operation) {
+//                    case "(":
+//                        return;
+//                    case "+":
+//                        return;
+//                    case "-":
+//                        return;
+//
+//
+//                }
+//
+//
+//                if (expression.charAt(countOperation) == '(') {
+//                    recurse(expression.substring(1, expression.length() - 1), countOperation);
+//                }
+//                double rightOperand
+
+
+
+            }
+        }
     }
 
     public Solution() {
-        //don't delete
-    }
+            //don't delete
+        }
+    
+
+
 }
